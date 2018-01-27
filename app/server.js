2fabca468b24c79b2fd767381f9bd0a6162f094f
EdwardAndress <eddie_andress@hotmail.com>
2018-01-27 15:26:32 +0000

    cohort selector and random picker MVP

--- app/server.js
+++ app/server.js
@@ -6,10 +6,23 @@ render = require('express-render')(app);
 
 app.set('view engine', 'pug')
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
+  cohorts = ['October', 'November', 'December']
+  res.render('cohorts', { title: 'Pick a cohort', cohorts: cohorts })
+})
+
+app.get('/october', function(req, res){
   var students = YAML.load('example_names.yml');
   var random_student = students.october[Math.floor(Math.random()*students.october.length)]
-  res.render('index', { title: 'Student picker', random_student: random_student })
+  var action = '/october'
+  res.render('random_student', { title: 'Pick a student', random_student: random_student, action: action })
+})
+
+app.get('/november', function(req, res){
+  var students = YAML.load('example_names.yml');
+  var random_student = students.november[Math.floor(Math.random()*students.november.length)]
+  var action = '/november'
+  res.render('random_student', { title: 'Pick a student', random_student: random_student, action: action })
 })
 
 app.listen(3000);
