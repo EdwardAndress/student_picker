5eb651b7b843a77ed554d849897b5db17146b000
EdwardAndress <eddie_andress@hotmail.com>
2018-01-29 12:32:17 +0000

    DRY up controller and views

--- app/server.js
+++ app/server.js
@@ -1,27 +1,23 @@
 var express = require("express");
-var app = express();
-YAML = require('yamljs');
+var app     = express();
+YAML        = require('yamljs');
 
 render = require('express-render')(app);
 
 app.set('view engine', 'pug')
 
 app.get('/', function(req, res) {
-  cohorts = ['October', 'November', 'December']
+  cohorts = Object.keys(YAML.load('example_names.yml'))
   res.render('cohorts', { title: 'Pick a cohort', cohorts: cohorts })
 })
 
-app.get('/october', function(req, res){
-  var students = YAML.load('example_names.yml');
-  var random_student = students.october[Math.floor(Math.random()*students.october.length)]
-  var action = '/october'
-  res.render('random_student', { title: 'Pick a student', random_student: random_student, action: action })
-})
+app.get('/cohorts/:cohort', function(req, res){
+  var cohort_selection = req.params.cohort
+  var students         = YAML.load('example_names.yml');
+  var cohort           = students[cohort_selection]
+  var random_student   = cohort[Math.floor(Math.random() * cohort.length)]
+  var action           = `/cohorts/${cohort_selection}`
 
-app.get('/november', function(req, res){
-  var students = YAML.load('example_names.yml');
-  var random_student = students.november[Math.floor(Math.random()*students.november.length)]
-  var action = '/november'
   res.render('random_student', { title: 'Pick a student', random_student: random_student, action: action })
 })
 
