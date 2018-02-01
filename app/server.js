354037564e37fc826eaa03757262eb02f6c1494f
EdwardAndress <eddie_andress@hotmail.com>
2018-02-01 11:34:57 +0000

    spike styling

--- app/server.js
+++ app/server.js
@@ -1,24 +1,30 @@
-var express = require("express");
-var app     = express();
-YAML        = require('yamljs');
-
-render = require('express-render')(app);
-
+var express = require("express")
+var app     = express()
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'pug')
 
+YAML   = require('yamljs')
+render = require('express-render')(app)
+
 app.get('/', function(req, res) {
-  cohorts = Object.keys(YAML.load('example_names.yml'))
+  cohorts = Object.keys(YAML.load('students.yml'))
   res.render('cohorts', { title: 'Pick a cohort', cohorts: cohorts })
 })
 
 app.get('/cohorts/:cohort', function(req, res){
+  var students         = YAML.load('students.yml')
   var cohort_selection = req.params.cohort
-  var students         = YAML.load('example_names.yml');
   var cohort           = students[cohort_selection]
-  var random_student   = cohort[Math.floor(Math.random() * cohort.length)]
+  var random_student   = cohort[random_index(cohort)]
   var action           = `/cohorts/${cohort_selection}`
 
   res.render('random_student', { title: 'Pick a student', random_student: random_student, action: action })
 })
 
-app.listen(3000);
+function random_index(cohort) {
+  // shuffle and then pick from the start
+  return Math.floor(Math.random() * cohort.length)
+}
+
+app.listen(3000)
--- test/server.js
+++ test/server.js
@@ -10,3 +10,37 @@ describe('GET "/"', function() {
     })
   })
 })
+
+describe('GET "/cohorts/:cohort"', function() {
+  var cohort;
+  function url(cohort) { return `http://localhost:3000/cohorts/${cohort}` }
+
+  describe("when data includes cohorts for October, Novemver and December", function() {
+    describe('passing "october"', function() {
+      it("returns status code 200", function(done) {
+        request(url('october'), function(error, response, body) {
+          expect(response.statusCode).to.eq(200)
+          done()
+        })
+      })
+    })
+
+    describe('passing "november"', function() {
+      it("returns status code 200", function(done) {
+        request(url('november'), function(error, response, body) {
+          expect(response.statusCode).to.eq(200)
+          done()
+        })
+      })
+    })
+
+    describe('passing "december"', function() {
+      it("returns status code 200", function(done) {
+        request(url('december'), function(error, response, body) {
+          expect(response.statusCode).to.eq(200)
+          done()
+        })
+      })
+    })
+  })
+})
